b4b080c833df570a0ed6f54fab35ada5b6d8575a
Ivan Eggel <ivan.eggel@gmail.com>
2024-12-16 10:00:35 +0100

    Fix typo in filename

--- src/test/java/ch/hevs/medgift/imageclef2025/registration/config/AI4MediaFieldsTest.java
+++ src/test/java/ch/hevs/medgift/imageclef2025/registration/config/AI4MediaFieldsTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-public class AI4MediaFieldsText {
+public class AI4MediaFieldsTest {
 
 	@Test
 	public void shouldReturnFieldNamesByKey() {
